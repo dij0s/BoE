85f1f53e64c0f62296dfba23308cdbe7e3f44a7d
dij0s <redrouge03@gmail.com>
2023-06-12 10:52:02 +0200

    some psrites

--- src/ch/hevs/boe/stage/room/predefined/SpawnRoom.scala
+++ src/ch/hevs/boe/stage/room/predefined/SpawnRoom.scala
@@ -7,7 +7,7 @@ import ch.hevs.boe.stage.room.{Room, Wall}
 
 import scala.collection.mutable.HashMap
 
-class SpawnRoom extends Room("data/sprites/cave_room.png") {
+class SpawnRoom extends Room("data/sprites/cave_room_spawn.png") {
 	borders = HashMap(Directions.LEFT -> new Wall(new Position(0,0), 100, 600, Directions.LEFT),
 		Directions.BOTTOM -> new Wall(new Position(100,500), 700, 100, Directions.BOTTOM),
 		Directions.RIGHT -> new Wall(new Position(800,0), 100, 600, Directions.RIGHT),
