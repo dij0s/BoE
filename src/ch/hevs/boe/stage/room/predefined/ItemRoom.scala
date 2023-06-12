2f304157d2380d913546260f596f929cee905474
dij0s <redrouge03@gmail.com>
2023-06-12 09:41:55 +0200

    hotfix

--- src/ch/hevs/boe/stage/room/predefined/ItemRoom.scala
+++ src/ch/hevs/boe/stage/room/predefined/ItemRoom.scala
@@ -12,8 +12,5 @@ class ItemRoom extends Room("data/sprites/item_room.png") {
 		Directions.RIGHT -> new Wall(new Position(800, 0), 100, 600, Directions.RIGHT),
 		Directions.TOP -> new Wall(new Position(100, 0), 700, 100, Directions.TOP))
 
-	override def _init(): Unit = {
-		super._init()
-		println("Item room initied !!!aw")
-	}
+	override def _init(): Unit = super._init()
 }
